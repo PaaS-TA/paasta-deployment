fd6d3b197fcb9d864efc993483871de968e7fd3b
okpc579 <ski579579@naver.com>
2021-01-29 00:22:01 +0900

    remove -l aws-vars.yml

--- paasta/deploy-aws-external-blobstore-minio.sh
+++ paasta/deploy-aws-external-blobstore-minio.sh
@@ -4,14 +4,11 @@ BOSH_ENVIRONMENT= "${BOSH_ENVIRONMENT}"			 # bosh director alias name (PaaS-TAì—
 
 bosh -e ${BOSH_ENVIRONMENT} -d paasta -n deploy paasta-deployment.yml \
 	-o operations/aws.yml \
-	-o operations/use-compiled-releases-online.yml \
 	-o operations/use-haproxy.yml \
 	-o operations/use-haproxy-public-network.yml \
-	-o operations/use-compiled-releases-haproxy-online.yml \
 	-o operations/use-postgres.yml \
-	-o operations/use-compiled-releases-postgres-online.yml \
 	-o operations/rename-network-and-deployment.yml \
 	-o operations/use-external-blobstore-minio.yml \
 	-l operations/example-vars-files/vars-use-external-blobstore-minio.yml \
-	-l aws-vars.yml \
+	-l vars.yml \
 	-l ../../common/common_vars.yml
