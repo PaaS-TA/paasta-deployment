943e5298cd025b13916ef703548ef005410cc33d
okpc579 <55691511+okpc579@users.noreply.github.com>
2021-02-02 09:59:12 +0900

    deployment 주석 수정

--- paasta/deploy-aws-7vms.sh
+++ paasta/deploy-aws-7vms.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-BOSH_ENVIRONMENT="${BOSH_ENVIRONMENT}"                   # bosh director alias name (PaaS-TA에서 제공되는 create-bosh-login.sh 미 사용시 bosh envs에서 이름을 확인하여 입력) -d paasta -n deploy min-paasta-deployment.yml \
+BOSH_ENVIRONMENT="${BOSH_ENVIRONMENT}"                   # bosh director alias name (PaaS-TA에서 제공되는 create-bosh-login.sh 미 사용시 bosh envs에서 이름을 확인하여 입력)
 
 bosh -e ${BOSH_ENVIRONMENT} -d paasta -n deploy min-paasta-deployment.yml \
         -o operations/min-aws.yml \
