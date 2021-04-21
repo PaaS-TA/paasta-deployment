082ef47f6cca74e350b44d9bb34b84edd042816d
jinhyojin <hjinjin@crossent.com>
2021-04-21 10:55:12 +0900

    Update CCE - paasta

--- paasta/deploy-aws.sh
+++ paasta/deploy-aws.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-BOSH_ENVIRONMENT="${BOSH_ENVIRONMENT}"			 # bosh director alias name (PaaS-TA에서 제공되는 create-bosh-login.sh 미 사용시 bosh envs에서 이름을 확인하여 입력)
-
-bosh -e ${BOSH_ENVIRONMENT} -d paasta -n deploy paasta-deployment.yml \
+bosh -e micro-bosh -d paasta -n deploy paasta-deployment.yml \
 	-o operations/aws.yml \
-	-o operations/cce.yml \
+	-o operations/cce-postgres.yml \
+	-o operations/use-compiled-releases-online.yml \
 	-o operations/use-haproxy.yml \
 	-o operations/use-haproxy-public-network.yml \
+	-o operations/use-compiled-releases-haproxy-online.yml \
 	-o operations/use-postgres.yml \
+	-o operations/use-compiled-releases-postgres-online.yml \
 	-o operations/rename-network-and-deployment.yml \
-	-l vars.yml \
+	-l aws-vars.yml \
 	-l ../../common/common_vars.yml
