875f27484cc6da0e7a2cea89644cc25b6835d684
okpc579 <>
2021-04-30 05:16:25 +0000

    modfiy offline release, deploy.sh

--- paasta/deploy-azure.sh
+++ paasta/deploy-azure.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-bosh -e micro-bosh -d paasta -n deploy paasta-deployment.yml \
-        -o operations/azure.yml \
-        -o operations/cce-postgres.yml \
-        -o operations/use-compiled-releases-online.yml \
-        -o operations/use-haproxy.yml \
-        -o operations/use-haproxy-public-network.yml \
-        -o operations/use-compiled-releases-haproxy-online.yml \
-        -o operations/use-postgres.yml \
-        -o operations/use-compiled-releases-postgres-online.yml \
-        -o operations/rename-network-and-deployment.yml \
-        -l azure-vars.yml \
-        -l ../../common/common_vars.yml
+BOSH_ENVIRONMENT="${BOSH_ENVIRONMENT}"			 # bosh director alias name (PaaS-TA에서 제공되는 create-bosh-login.sh 미 사용시 bosh envs에서 이름을 확인하여 입력)
+
+bosh -e ${BOSH_ENVIRONMENT} -d paasta -n deploy paasta-deployment.yml \
+	-o operations/azure.yml \
+	-o operations/use-haproxy.yml \
+	-o operations/use-haproxy-public-network.yml \
+	-o operations/use-postgres.yml \
+	-o operations/rename-network-and-deployment.yml \
+	-l vars.yml \
+	-l ../../common/common_vars.yml
--- paasta/deploy-gcp.sh
+++ paasta/deploy-gcp.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-bosh -e micro-bosh -d paasta deploy paasta-deployment.yml \
-        -o operations/cce-postgres.yml \
-        -o operations/use-compiled-releases-online.yml \
-        -o operations/use-haproxy.yml \
-        -o operations/use-haproxy-public-network.yml \
-        -o operations/use-compiled-releases-haproxy-online.yml \
-        -o operations/use-postgres.yml \
-        -o operations/use-compiled-releases-postgres-online.yml \
-        -o operations/rename-network-and-deployment.yml \
-        -l gcp-vars.yml \
-        -l ../../common/common_vars.yml
+BOSH_ENVIRONMENT="${BOSH_ENVIRONMENT}"			 # bosh director alias name (PaaS-TA에서 제공되는 create-bosh-login.sh 미 사용시 bosh envs에서 이름을 확인하여 입력)
+
+bosh -e ${BOSH_ENVIRONMENT} -d paasta deploy paasta-deployment.yml \
+	-o operations/use-haproxy.yml \
+	-o operations/use-haproxy-public-network.yml \
+	-o operations/use-postgres.yml \
+	-o operations/rename-network-and-deployment.yml \
+	-l vars.yml \
+	-l ../../common/common_vars.yml
