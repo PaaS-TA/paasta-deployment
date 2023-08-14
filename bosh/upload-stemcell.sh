ddc69c3387ab1649f688662289b66002ea05d154
moonii <zinta@naver.com>
2023-08-14 18:35:46 +0900

    patch oss-0801

--- bosh/upload-stemcell.sh
+++ bosh/upload-stemcell.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-BIONIC_STEMCELL_VERSION=1.195
-JAMMY_STEMCELL_VERSION=1.108
+BIONIC_STEMCELL_VERSION=1.204
+JAMMY_STEMCELL_VERSION=1.181
 CURRENT_IAAS="${CURRENT_IAAS}"                                   # IaaS Information (PaaS-TA에서 제공되는 create-bosh-login.sh 미 사용시 aws/azure/gcp/openstack/vsphere/bosh-lite 입력)
 BOSH_ENVIRONMENT="${BOSH_ENVIRONMENT}"                   # bosh director alias name (PaaS-TA에서 제공되는 create-bosh-login.sh 미 사용시 bosh envs에서 이름을 확인하여 입력)
 
